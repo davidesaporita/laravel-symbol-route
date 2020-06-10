e903bab7316c6c9b43e1bdd65e96f23c1b2c72e5
Davide Saporita <davide.saporita@gmail.com>
2020-06-10 18:11:35 +0200

    Added jquery & transition

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+$(document).ready(function() {
+    $('.bio').toggleClass('show');
+    $('.links').click(function() { 
+        $('.bio').toggleClass('show');
+    });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,6 @@
 @extends('layouts.main')
 @section('content')
     <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
+        <h1>Seleziona un attore dal menu e accedi alla sua biografia.</h1>
     </div>
 @endsection
